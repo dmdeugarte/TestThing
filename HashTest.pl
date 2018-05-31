1c045b8e690314fcc33c2c10238a20d4965dc504
Damian <ddeugarte022@rsdmo.org>
2018-05-31 11:38:50 -0500

    Added Hash Practice file

---
+++ HashTest.pl
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+
+my %scientists = (
+	"Newton" => "Isaac",
+	"Einstein" => "Albert", 
+	"Darwin" => "Charles",
+);
+
+print $scientists{"Newton"}; #"Isaac"
+
+#use [] to retrieve value from array, and braces from hash
+
+
