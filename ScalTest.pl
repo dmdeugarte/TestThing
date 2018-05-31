4abcb57536010fcc496be936fccc7e04937894d4
Damian <ddeugarte022@rsdmo.org>
2018-05-31 11:01:26 -0500

    Added a Scalar Practice Perl

---
+++ ScalTest.pl
@@ -0,0 +1,15 @@
+use strict;
+use warnings;
+
+my $undef = undef;
+print $undef; # THIS IS A COMMENT 
+
+#implicit undef:
+my $undef2;
+print $undef2;
+
+my $num = 40440.5;
+print $num;
+
+my $string = "world";
+print $string;
