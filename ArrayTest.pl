3c682fbb2152f5f46bc81e57a8a3ca7103e30b68
Damian <ddeugarte022@rsdmo.org>
2018-05-31 11:27:11 -0500

    Second Update of Array Practice file

--- ArrayTest.pl
+++ ArrayTest.pl
@@ -12,3 +12,10 @@ my @array = (
 );
 
 print $array[1]; # will print these
+
+print "This Array has ".(scalar @array)." elements"; #Will print how many elements there are
+
+print "@array" #will print all elements of the array
+
+#interpolation(looking for something that isn't there can be 
+#avoided by using a \ or ' ' instead.
